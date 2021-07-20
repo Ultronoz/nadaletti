e11ae1f60d5669307b0d6320a16c824104b28ccb
Ultronoz <diegonadalettiult@gmail.com>
2021-07-20 15:49:19 -0300

    nada de mais

--- nadaletti/styles/js.js
+++ nadaletti/styles/js.js
@@ -1,12 +1,18 @@
 var janelaMenu = document.getElementById('lista-menu');
+var heroArea = document.getElementById('hero-area');
 var valor = true;
-
+window.onload = function(){
+    heroArea.style.zIndex = '10';
+}
 function mostra() {
     if (valor == false) {
+        heroArea.style.zIndex = '10';
         janelaMenu.style.display = 'none';
         valor = true; 
+        
     }
     else {
+        heroArea.style.zIndex = '-1';
         janelaMenu.style.display = 'block';
         valor = false;
         console.log("mosr");
@@ -15,3 +21,6 @@ function mostra() {
     
 };
 
+function contact(){
+    alert('funciona');
+}
